1430b8ce43d811c2d941d3392c975d3405a5f3bb
BBonifield <bobbonifield@gmail.com>
2018-11-11 15:34:34 -0700

    Finishing setup

--- check.js
+++ check.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const dt = require('node-datetime');
 const rp = require('request-promise');
 const twilio = require('twilio');
 const Promise = require('bluebird');
+const moment = require('moment');
 
 const connectDb = require('./src/connect-db');
 
@@ -36,7 +36,7 @@ const authUrl = `${process.env.API_HOST}/v1/owners/auth`;
   });
 
   const authToken = authResp.data.token;
-  const today = dt.create().format('Y-m-d');
+  const today = moment().format('YYYY-MM-DD');
   const reservationsUri = `${process.env.API_HOST}/v1/owners/${process.env.VACASA_OWNER_ID}/units/${process.env.VACASA_UNIT_ID}/reservations`
 
   const reservationResp = await rp({
@@ -56,22 +56,35 @@ const authUrl = `${process.env.API_HOST}/v1/owners/auth`;
     const records = await collection.find({ id: reservation.id }).toArray();
 
     if (records.length === 0) {
-      // new record found
-      console.log('new record found');
-      try {
-        await collection.insertOne(reservation);
-      } catch(err) {
-        console.log(err);
-      }
-    } else {
-      // skip - no-op
+      console.log('New reservation found');
+
+      const startDate = moment(reservation.attributes.start_date);
+      const endDate = moment(reservation.attributes.end_date);
+      const nights = endDate.diff(startDate, 'days');
+      const pricePerNight = (parseFloat(reservation.attributes.total_rent) / nights.toFixed(2)).toFixed(2);
+
+      const smsTargets = process.env.SMS_TARGETS.split(',');
+      const body = `${reservation.attributes.first_name} is getting cozy!\n` +
+        `New booking for ${nights} nights @ ${pricePerNight > 0 ? pricePerNight : 'free'}/night!\n` +
+        `Start date: ${startDate.format('dddd, MMMM Do')}\n` +
+        `End date: ${endDate.format('dddd, MMMM Do')}\n` +
+        `Gross rent: $${reservation.attributes.total_rent}`;
+
+      await Promise.all(smsTargets.map(async (smsTarget) => {
+        try {
+          await twilioClient.messages.create({
+            body: body,
+            to: smsTarget,
+            from: process.env.TWILIO_FROM,
+          });
+        } catch(err) {
+          console.log('Encountered error:', err);
+        }
+      }));
+
+      await collection.insertOne(reservation);
     }
   }));
 
   client.close();
 })();
-
-
-
-
-// `${process.env.API_HOST}/v1/owners/${process.env.VACASA_OWNER_ID}/units/${process.env.VACASA_UNIT_ID}/reservations?filter[unit]=${process.env.VACASA_UNIT_ID}&filter[current_date]=2018-11-11&sort=FirstNight`
